03ec6f64342e7dd7aba98b4350e87f5938463bbd
ootang2018 <yutang.liu2018@outlook.com>
2020-06-30 19:29:26 +0200

    rosparam

--- stable_baseline/script/main.py
+++ stable_baseline/script/main.py
@@ -18,34 +18,6 @@ from stable_baselines import SAC
 from env.blimp import BlimpEnv
 
 
-class PlottingCallback(BaseCallback):
-    """
-    Callback for plotting the performance in realtime.
-
-    :param verbose: (int)
-    """
-    def __init__(self, verbose=1):
-        super(PlottingCallback, self).__init__(verbose)
-        self._plot = None
-
-    def _on_step(self) -> bool:
-        # get the monitor's data
-        x, y = ts2xy(load_results(log_dir), 'timesteps')
-        if self._plot is None: # make the plot
-            plt.ion()
-            fig = plt.figure(figsize=(6,3))
-            ax = fig.add_subplot(111)
-            line, = ax.plot(x, y)
-            self._plot = (line, ax, fig)
-            plt.show()
-        else: # update and rescale the plot
-            self._plot[0].set_data(x, y)
-            self._plot[-2].relim()
-            self._plot[-2].set_xlim([self.locals["total_timesteps"] * -0.02, 
-                                     self.locals["total_timesteps"] * 1.02])
-            self._plot[-2].autoscale_view(True,True,True)
-            self._plot[-1].canvas.draw()
-
 class SaveOnBestTrainingRewardCallback(BaseCallback):
     """
     Callback for saving a model (the check is done every ``check_freq`` steps)
@@ -63,31 +35,31 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
         self.save_path = os.path.join(log_dir, 'best_model')
         self.best_mean_reward = -np.inf
 
-    def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
+    # def _init_callback(self) -> None:
+    #     # Create folder if needed
+    #     if self.save_path is not None:
+    #         os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
 
-          # Retrieve training reward
-          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-          if len(x) > 0:
-              # Mean training reward over the last 100 episodes
-              mean_reward = np.mean(y[-100:])
-              if self.verbose > 0:
-                print("Num timesteps: {}".format(self.num_timesteps))
-                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+	        # Retrieve training reward
+	        x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+	        if len(x) > 0:
+	            # Mean training reward over the last 100 episodes
+	            mean_reward = np.mean(y[-100:])
+	            if self.verbose > 0:
+	              print("Num timesteps: {}".format(self.num_timesteps))
+	              print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
 
-              # New best model, you could save the agent here
-              if mean_reward > self.best_mean_reward:
-                  self.best_mean_reward = mean_reward
-                  # Example for saving best model
-                  if self.verbose > 0:
-                    print("Saving new best model at {} timesteps".format(x[-1]))
-                    print("Saving new best model to {}.zip".format(self.save_path))
-                  self.model.save(self.save_path)
+	            # New best model, you could save the agent here
+	            if mean_reward > self.best_mean_reward:
+	                self.best_mean_reward = mean_reward
+	                # Example for saving best model
+	                if self.verbose > 0:
+	                  print("Saving new best model at {} timesteps".format(x[-1]))
+	                  print("Saving new best model to {}.zip".format(self.save_path))
+	                self.model.save(self.save_path)
 
         return True
 
@@ -97,12 +69,14 @@ def main(logdir):
 
 	N_EPISODE = 5 
 	EPISODE_LENGTH = SLEEP_RATE*60 #60 sec
+	TOTAL_TIMESTEPS = EPISODE_LENGTH * N_EPISODE
 
 	# logdir
-	logdir = os.path.join(logdir, strftime("%Y-%m-%d--%H:%M:%S", localtime()))	
-	final_model_path = logdir+'/final_model'
-	checkpoint_model_path = logdir+'/model'
-	best_model_path = logdir+'/best_model'
+	logdir = os.path.join(logdir, strftime("%Y-%m-%d--%H:%M:%S", localtime()))
+	os.makedirs(logdir)	
+	checkpoint_path = os.path.join(logdir,'checkpoint')
+	callback_path = logdir
+	final_model_path = logdir+'final_model'
 
 	# env
 	env = BlimpEnv(SLEEP_RATE)
@@ -113,13 +87,13 @@ def main(logdir):
 	print("Action space:", env.action_space)
 
 	# callback
-	SAVE_FREQ = EPISODE_LENGTH*5 # every 5 episode
+	SAVE_FREQ = EPISODE_LENGTH*1 # every 1 episode
 	checkpoint_callback = CheckpointCallback(save_freq=SAVE_FREQ, 
-											save_path=checkpoint_model_path,
-	                                        name_prefix='sac_model')
+											save_path=checkpoint_path,
+	                                        name_prefix='sac_callback_model')
 	save_on_best_training_reward_callback = SaveOnBestTrainingRewardCallback(
 											check_freq=SAVE_FREQ, 
-											log_dir = best_model_path)
+											log_dir = callback_path)	
 	callback = CallbackList([
 		checkpoint_callback, 
 		save_on_best_training_reward_callback])
@@ -133,13 +107,12 @@ def main(logdir):
 		_init_setup_model=True)
 
 	print("---------- Start Learing -----------")
-	model.learn(total_timesteps=N_EPISODE*EPISODE_LENGTH, 
+	model.learn(total_timesteps=TOTAL_TIMESTEPS, 
 				log_interval=SAVE_FREQ, 
 				callback=callback)
 	
 	print("---------- Finish Learning ----------")
 	model.save(final_model_path)
-
 	del model # remove to demonstrate saving and loading
 	model = SAC.load(final_model_path)
 
@@ -150,8 +123,10 @@ def main(logdir):
 	    obs, rewards, dones, info = env.step(action)
 	    reward_sum += rewards
 
-	print("reward_sum=",reward_sum/100)
+	print("reward_sum=",reward_sum/SLEEP_RATE)
 
+	results_plotter.plot_results([logdir], TOTAL_TIMESTEPS, results_plotter.X_TIMESTEPS, "SAC BLIMP")
+	plt.show()
 
 if __name__ == "__main__":
 
--- stable_baseline/script/plotter.py
+++ stable_baseline/script/plotter.py
@@ -1,4 +1,6 @@
 from stable_baselines import results_plotter
 
 
-results_plotter.plot_results(["./log"], 10e6, results_plotter.X_TIMESTEPS, "Breakout")
+
+results_plotter.plot_results([log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
+plt.show()
