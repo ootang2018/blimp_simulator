8c99fcb7f1b74f7e656bfd4a4adb3da8442b3438
ootang2018 <yutang.liu2018@outlook.com>
2020-07-04 01:36:23 +0200

    update_reward

--- mbrl_dats/script/dats/dmbrl/config/blimp.py
+++ mbrl_dats/script/dats/dmbrl/config/blimp.py
@@ -114,12 +114,12 @@ class BlimpConfigModule:
 
         # define angle cost (phi, the)
         ang_cost = tf.math.reduce_mean(tf.abs(obs[:, 0:2]))
-        ang_cost = -tf.math.tanh(5*ang_cost, name=None)
+        ang_cost = -tf.math.tanh(2*ang_cost, name=None)
         # ang_mse_cost = tf.sqrt(tf.reduce_sum(tf.square(obs[:, 0:2]), axis=1)) # mse angle, not used
 
         # define direction cost (psi)
         dir_cost = tf.abs(obs[:, 2])
-        dir_cost = -tf.math.tanh(5*dir_cost, name=None)
+        dir_cost = -tf.math.tanh(2*dir_cost, name=None)
         # dir_abs_cost = tf.abs(obs[:, 2]) # psi angle, not used
 
         return w_dist*dist_cost + w_ang*ang_cost + w_dir*dir_cost 
@@ -130,7 +130,7 @@ class BlimpConfigModule:
 
         # define action cost
         act_cost = tf.norm(acs, ord='euclidean', axis=None, name=None)
-        act_cost = -tf.math.tanh(5*act_cost, name=None)
+        act_cost = -tf.math.tanh(2*act_cost, name=None)
         # act_mse_cost = tf.reduce_sum(tf.square(acs), axis=1) #mse action, not used
 
         return w_act*act_cost
